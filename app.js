7ce3b2a69234e86d7b7b59a7f3eb09f2b67c82f2
Stephan Allen <sallen202009@gmail.com>
2018-09-19 21:43:55 -0400

    put logic in a function

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 let url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json'
 url += '?' + $.param({
-  'api-key': "c68026da5e154175ab802b832de9e9a2"
+  'api-key': "c68026da5e154175ab802b832de9e9a2",
+})
+let numberOfRecords = 7;
+function printHeadlines(){
+    $.getJSON(url)
+    .done(data => {
+        let timesData = data.response.docs
+        console.log(timesData);
+        timesData.forEach((nytData, i) => {
+            if(i < numberOfRecords){
+                let headlines = $('<p>')
+                headlines.text(nytData.headline.main);
+                $('#pinHere').append(headlines);
+            }
+        });
+    })
+    .fail(data => {
+        console.log('there was a problem' + data);
+    });
+}
+
+$(document).ready(()=>{
+    printHeadlines();
 })
 
-$.getJSON(url)
-.done(function(data){
-    let timesData = data.response.docs[0].headline.main
-    $('#pinHere').text(timesData);
-})
-.fail(function(data){
-    console.log('there was a problem' + data);
-});
